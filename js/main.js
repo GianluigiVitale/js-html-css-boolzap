631c069085c5c9103e2ce5cc260afe5070d677ff
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-05 16:48:48 +0100

    when click send icon, send input value as message in the chat

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    $('.fa-paper-plane').click(function(){
+        var messaggioInput = $('#message').val();
+        $('#message').val('');
+        var messaggio = $('.template-box-message-sent .template-message-sent').clone();
+        messaggio.children('.testo-messaggio').text(messaggioInput);
+        $('.center-chat').append(messaggio);
+    })
 
 
 
