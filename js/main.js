624b5c5ed842f643592a0b53c00b99951a77a26a
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-05 16:57:47 +0100

    when you send a message, after 1 second you receive an 'Ok' message

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ $(document).ready(function() {
         var messaggio = $('.template-box-message-sent .template-message-sent').clone();
         messaggio.children('.testo-messaggio').text(messaggioInput);
         $('.center-chat').append(messaggio);
+
+        setTimeout(messageOk, 1000);
+        function messageOk() {
+            var messaggio1 = $('.template-box-message-received .template-message-received').clone();
+            messaggio1.children('.testo-messaggio').text('Ok');
+            $('.center-chat').append(messaggio1);
+        }
     })
+
+
 
 
 
