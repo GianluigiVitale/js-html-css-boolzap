106cf50ea1d2cad93b6c406c24f9122a857334ba
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-06 16:16:48 +0100

    when you click a user it changes top-chat name
    additional: impossible to send empty message

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     $('.fa-paper-plane').click(function(){
         var messaggioInput = $('#message').val();
-        $('#message').val('');
-        var messaggio = $('.template-box-message-sent .template-message-sent').clone();
-        messaggio.children('.testo-messaggio').text(messaggioInput);
-        $('.center-chat').append(messaggio);
+        if (messaggioInput.trim().length > 0) {
+            $('#message').val('');
+            var messaggio = $('.template-box-message-sent .template-message-sent').clone();
+            messaggio.children('.testo-messaggio').text(messaggioInput);
+            $('.center-chat').append(messaggio);
 
-        setTimeout(messageOk, 1000);
-        function messageOk() {
-            var messaggio1 = $('.template-box-message-received .template-message-received').clone();
-            messaggio1.children('.testo-messaggio').text('Ok');
-            $('.center-chat').append(messaggio1);
-            scrollDown();
+            setTimeout(messageOk, 1000);
         }
     });
+
+    function messageOk() {
+        var messaggio1 = $('.template-box-message-received .template-message-received').clone();
+        messaggio1.children('.testo-messaggio').text('Ok');
+        $('.center-chat').append(messaggio1);
+        scrollDown();
+    }
 
     $("#message").keypress(function(event) {
         if (event.keyCode === 13) {
@@ -26,6 +29,10 @@ $(document).ready(function() {
         var centerChatDown = $('.center-chat').height();
         $('.center-chat').scrollTop(centerChatDown);
     }
+    // function scrollDown() {
+    //     var centerChatDown = document.getElementById('center-chat');
+    //     centerChatDown.scrollTop = 10000;
+    // }
 
 
     $('#search-bar').keyup(function(event){
@@ -49,6 +56,16 @@ $(document).ready(function() {
         $('.fa-microphone').removeClass('not-active');
     });
 
+    $('.username').click(function() {
+        var nomeUtenteClk = $(this).find('.name-recent-user h3').text();
+        var utente = $(this).data('codiceUtente');
+        $('.img-name-lastaccess .img-time').each(function() {
+            if (utente == $(this).data('codiceUtente')) {
+                $('.img-name-lastaccess .img-time').addClass('not-active');
+                $(this).removeClass('not-active');
+            }
+        });
+    });
 
 
 
