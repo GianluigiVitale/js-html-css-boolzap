c626b6491fde25ff7be2ff55a57cf36179e37f38
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-29 13:08:03 +0200

    Added features: (see desc.)
    -Added time (00:00) to messages, time is not fixed but uses the current hour
    -The last message of a chat appears on the left side under the name of the selected user as well as the time when the last message was sent
    -If the message is too long it's cropped to the given length
    -Added different background color to active chat

--- js/main.js
+++ js/main.js
@@ -24,29 +24,17 @@ $(document).ready(function() {
     //     });
     // }
 
-    var elencoRisposte = [
-        'Chi lascia la strada vecchia per la nuova arriva prima perché è meglio asfaltata.',
-        'Gallina vecchia si fa il lifting.',
-        'Quando si chiude una porta, tu rimani fuori.',
-        'Chi va piano va sano, ma viene tamponato poco lontano.',
-        'Chi troppo vuole nulla rifiuta.',
-        'L’ospite è come il pesce, dopo tre giorni… nuota.',
-        'Can che abbaia lo sbattono fuori dal condominio.',
-        'Chi di spada ferisce gli danno 10 anni senza condizionale.',
-        'Chi dorme non piglia sonniferi.',
-        'Non c’è peggior sordo di chi non sente veramente.',
-    ]
-
     $('.fa-paper-plane').click(function(){          // al click dell'icona paper-plane
         var messaggioInput = $('#message').val();
         if (messaggioInput.trim().length > 0) {         // se l'input ha contenuto
             $('#message').val('');                      // resetto il valore dell'input
-            invioMessaggioConScrollDown(messaggioInput, 'sent', '.center-chat.active');    // mando un messaggio in chat
+            invioMessaggioConScrollDown(messaggioInput, 'sent', '.center-chat.active', oraAttuale());    // mando un messaggio in chat
+                aggiornamentoMessaggioEOrario(messaggioInput)
             checkInputMessage();                                //Eseguo nuovamente il controllo per rimuovere il paper-plane
-            setTimeout(function() {
-                var numeroRispostaRandom = generaRandom(0, 9);
-                var rispostaRandom = elencoRisposte[numeroRispostaRandom];
-                invioMessaggioConScrollDown(rispostaRandom, 'received', '.center-chat.active');
+            setTimeout(function() {                             // dopo un secondo ricevo una risposta casuale scelta tra l'array elencoRisposte
+                var rispostaRandom = generaRispostaRandom();
+                invioMessaggioConScrollDown(rispostaRandom, 'received', '.center-chat.active', oraAttuale());
+                    aggiornamentoMessaggioEOrario(rispostaRandom)
             }, 1000);
         }
     });
@@ -59,7 +47,9 @@ $(document).ready(function() {
     });
 
 
-    $('.username').click(function() {       // al click del div (.username) cambio il nome sopra la chat e la chat
+    $('.username').click(function() {       // al click del div (.username) aggiungo a this la classe selected e cambio il nome sopra la chat e la chat
+        $('.username').removeClass('selected');
+        $(this).addClass('selected');
         var thisAvatar = $(this).find('img').attr('src');                   // Creo una variabile con il valore del src dell'immagine dell'avatar cliccato
         var thisName = $(this).find('h3').text();                           // Variabile con il Nome dell'utente cliccato
         var thisHour = $(this).find('.last-chat-user p').text();            // Variabile con l'orario dell'ultimo accesso dell'utente cliccato
@@ -104,105 +94,125 @@ $(document).ready(function() {
         $(this).parent().remove();
     });
 
-    var messaggiArchiviati = {
+    var messaggiArchiviati = {      // questo oggetto contiene i messaggi di ogni conversazione (che poi viene dato in pasto a handlebars)
         c0: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Fabio come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Purtroppo è andata così',
+                sentReceived: 'sent',
+                orario: '16:48'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Non poteva essere altrimenti',
+                sentReceived: 'received',
+                orario: '17:53'
             }
         ],
         c1: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Fabrizio come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Ok, capito',
+                sentReceived: 'sent',
+                orario: '23:56'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Ti devo raccontare una cosa',
+                sentReceived: 'received',
+                orario: '08:07'
             }
         ],
         c2: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Marta come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'E quindi hai trovato lavoro?',
+                sentReceived: 'sent',
+                orario: '00:35'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Proprio così',
+                sentReceived: 'received',
+                orario: '00:35'
             }
         ],
         c3: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Irene come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Sono felice',
+                sentReceived: 'sent',
+                orario: '12:37'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Beato te!',
+                sentReceived: 'received',
+                orario: '12:38'
             }
         ],
         c4: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
+            {
                 testoMessaggio: 'Ciao Gianluca come stai?',
-                sentReceived: 'sent'
+                sentReceived: 'sent',
+                orario: '11:59'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Bene, tu?',
+                sentReceived: 'received',
+                orario: '23:29'
             }
         ],
         c5: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Luca come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Ma te lo sei comprato il nuovo pc alla fine?',
+                sentReceived: 'sent',
+                orario: '17:11'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Si',
+                sentReceived: 'received',
+                orario: '18:15'
             }
         ],
         c6: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Riccardo come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Anna mi ha lasciato :(',
+                sentReceived: 'sent',
+                orario: '14:59'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Ma stai scherzando?!',
+                sentReceived: 'received',
+                orario: '15:00'
             }
         ],
         c7: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Nicolò come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Dopo qualche giorno sono stato meglio',
+                sentReceived: 'sent',
+                orario: '15:00'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Meno male',
+                sentReceived: 'received',
+                orario: '17:27'
             }
         ],
         c8: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
+            {
                 testoMessaggio: 'Ciao Michele come stai?',
-                sentReceived: 'sent'
+                sentReceived: 'sent',
+                orario: '19:11'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Tutto bene grazie, tu?',
+                sentReceived: 'received',
+                orario: '19:16'
             }
         ],
         c9: [
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Ciao Chiara come stai?',
-                sentReceived: 'sent'
+            {
+                testoMessaggio: 'Non ti preoccupare, vedrai che ce la farai',
+                sentReceived: 'sent',
+                orario: '00:56'
             },
-            {                                 // Assemblo in un oggetto il contenuto del messaggio
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
-                sentReceived: 'received'
+            {
+                testoMessaggio: 'Ti ringrazio',
+                sentReceived: 'received',
+                orario: '12:11'
             }
         ],
     };
@@ -211,18 +221,17 @@ $(document).ready(function() {
     var template = Handlebars.compile(source);                 // do in pasto ad Handlebars il template clonato
 
     for (var convKey in messaggiArchiviati) {                            // ciclo nell'oggetto
-        // console.log(convKey);                                         // Stampo la chiave
-        // console.log(messaggiArchiviati[convKey]);                     // Stampo il valore MEMENTO senza dot notation
         var numeroConversazione = convKey[1];
         // console.log('Trasformo la chiave: ' + convKey + ' in: ----> ' + numeroConversazione);
-        for (var i = 0; i < convKey.length; i++) {                       // per ogni chiave dell'oggetto devo fare un ciclo nell'array corrisponte
+        for (var i = 0; i < convKey.length; i++) {                       // per ogni chiave dell'oggetto faccio un ciclo nell'array
             // console.log(messaggiArchiviati[convKey][i]);
-            var oggettoMessaggio = messaggiArchiviati[convKey][i];       // Shortcut
-            var testoMessaggio = oggettoMessaggio.testoMessaggio;        // Shortcut
-            var direzione = oggettoMessaggio.sentReceived;                  // Shortcut
+            var oggettoMessaggio = messaggiArchiviati[convKey][i];
+            var testoMessaggio = oggettoMessaggio.testoMessaggio;       // salvo in delle variabili i vari contenuti della chiave
+            var direzione = oggettoMessaggio.sentReceived;
+            var orarioMessaggio = oggettoMessaggio.orario;
 
             var selettoreConversazione = $('.center-chat[data-codice-utente="' + numeroConversazione + '"]');
-            invioMessaggioConScrollDown(testoMessaggio, direzione, selettoreConversazione);
+            invioMessaggioConScrollDown(testoMessaggio, direzione, selettoreConversazione, orarioMessaggio);
         }
     }
 
@@ -237,7 +246,7 @@ $(document).ready(function() {
         {
             source: 'img/2.png',
             name: 'Fabrizio',
-            message: 'ti devo raccontare una cosa',
+            message: 'Ti devo raccontare una cosa',
             time: '08:07'
         },
         {
@@ -249,43 +258,43 @@ $(document).ready(function() {
         {
             source: 'img/4.png',
             name: 'Irene',
-            message: 'beato te!',
+            message: 'Beato te!',
             time: '12:38'
         },
         {
             source: 'img/5.png',
             name: 'Gianluca',
-            message: 'Come stai?',
+            message: 'Bene, tu?',
             time: '23:29'
         },
         {
             source: 'img/6.png',
             name: 'Luca',
-            message: 'si',
+            message: 'Si',
             time: '18:15'
         },
         {
             source: 'img/7.png',
             name: 'Riccardo',
-            message: 'ma stai scherzando?!',
+            message: 'Ma stai scherzando?!',
             time: '15:00'
         },
         {
             source: 'img/8.png',
             name: 'Nicolò',
-            message: 'meno male',
+            message: 'Meno male',
             time: '17:27'
         },
         {
             source: 'img/9.png',
             name: 'Michele',
-            message: 'tutto bene grazie, tu?',
+            message: 'Tutto bene grazie, tu?',
             time: '19:16'
         },
         {
             source: 'img/10.png',
             name: 'Chiara',
-            message: 'ti ringrazio',
+            message: 'Ti ringrazio',
             time: '12:11'
         }
     ]
@@ -307,11 +316,12 @@ $(document).ready(function() {
 
 
 
-    function invioMessaggioConScrollDown(messaggioDaInviare, sendOrReceiveClass, selettoreConversazione) {  // funzione che serve per mandare o ricevere un messaggio, valori di input messaggio da inserrire e se e' un messaggio da ricevere o da mandare
+    function invioMessaggioConScrollDown(messaggioDaInviare, sendOrReceiveClass, selettoreConversazione, orarioMessaggio) {  // funzione che serve per mandare o ricevere un messaggio, valori di input messaggio da inserrire, se e' un messaggio da ricevere o da mandare, la conversazione attuale, e l'orario corrente
         var datiMessaggio = {
             testoMessaggio: messaggioDaInviare,
             sentReceived: sendOrReceiveClass,
-            sxODx: 'null'
+            sxODx: 'null',
+            orario: orarioMessaggio
         }
         if (sendOrReceiveClass == 'sent') {
             datiMessaggio.sxODx = 'right';
@@ -324,6 +334,25 @@ $(document).ready(function() {
         scrollDown();
     }
 
+    function generaRispostaRandom() {       // questa funzione ritorna una risposta casuale presa da un elenco di risposte predefinite
+        var elencoRisposte = [
+            'Chi lascia la strada vecchia per la nuova arriva prima perché è meglio asfaltata.',
+            'Gallina vecchia si fa il lifting.',
+            'Quando si chiude una porta, tu rimani fuori.',
+            'Chi va piano va sano, ma viene tamponato poco lontano.',
+            'Chi troppo vuole nulla rifiuta.',
+            'L’ospite è come il pesce, dopo tre giorni… nuota.',
+            'Can che abbaia lo sbattono fuori dal condominio.',
+            'Chi di spada ferisce gli danno 10 anni senza condizionale.',
+            'Chi dorme non piglia sonniferi.',
+            'Non c’è peggior sordo di chi non sente veramente.',
+        ];
+
+        var numeroRispostaRandom = generaRandom(0, 9);
+        var rispostaRandom = elencoRisposte[numeroRispostaRandom];
+
+        return rispostaRandom;
+    }
 
     function ricercaUtente(filtroCaratteri) {   // Solo se i caratteri digitati nell'input sono inclusi nel nome dell'utente allora lo mostra
         $('.username').each(function(){
@@ -336,7 +365,7 @@ $(document).ready(function() {
         });
     }
 
-    function checkInputMessage() {  //Funzione che controlla se l'input ha un messaggio e fa apparire e scomparire le icone
+    function checkInputMessage() {      // Funzione che controlla se l'input ha un messaggio e fa apparire e scomparire le icone
         var messaggioInput = $('#message').val();
 
         if (messaggioInput.length > 0) {
@@ -346,6 +375,27 @@ $(document).ready(function() {
             $('.fa-microphone').removeClass('not-active')
             $('.fa-paper-plane').addClass('not-active')
         }
+    }
+
+    function oraAttuale() {         // questa funzione ritorna l'orario attuale
+        var dataCorrente = new Date();
+        var orarioAdesso = dataCorrente.getHours() + ":" + (dataCorrente.getMinutes() <10?'0':'') + dataCorrente.getMinutes();     // se getMinutes è minore di 10 aggiunge uno zero (per far si di avere sempre 2 cifre)
+        return orarioAdesso;
+    };
+
+    function tagliaMessaggio(messaggio) {       // questa funzione dato l'input messaggio se esso e' maggiore della lunghezza dei caratteriMax lo taglia
+        var caratteriMax = 28;
+        var controlloLunghezza = messaggio;
+        if (messaggio.length > caratteriMax) {
+            var taglio = messaggio.substr(0, caratteriMax)
+            var controlloLunghezza = taglio + '...';
+        }
+        return controlloLunghezza;
+    }
+
+    function aggiornamentoMessaggioEOrario(messaggioDaAggiornare) {     // questa funzione imposta l'ultimo messaggio inviato e l'orario attuale come testo nella sezione utente corrispondente
+        $('.username.selected').find('.name-recent-user p').text(tagliaMessaggio(messaggioDaAggiornare));
+        $('.username.selected').find('.last-chat-user p').text(oraAttuale());
     }
 
     function scrollDown() {         // funzione che serve per fare lo scroll automatico (all'invio o ricezione di un messaggio)
